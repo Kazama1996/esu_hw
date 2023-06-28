b8e1d20fe73888f416877d605ecb3de92b52830f
Y.kazama <Y.Kazama1996@gmail.com>
2023-06-28 18:00:09 +0800

    create SecurityConfig and modify the User entity

--- hw/src/main/java/com/esun/hw/model/User.java
+++ hw/src/main/java/com/esun/hw/model/User.java
@@ -2,6 +2,12 @@ package com.esun.hw.model;
 
 import java.util.UUID;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,17 +17,27 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID userId;
 
+    @Column(name = "user_name")
     private String userName;
 
+    @Column(name = "email")
     private String email;
 
+    @Column(name = "phone")
     private String phone;
 
+    @Column(name = "pwd")
     private String password;
 
+    @Column(name = "biography")
     private String biography;
 }

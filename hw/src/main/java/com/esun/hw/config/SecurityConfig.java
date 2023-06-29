a4b6a22f00b328c140310571b00b2e8d30de592f
Y.kazama <Y.Kazama1996@gmail.com>
2023-06-29 19:53:01 +0800

    create a jwt based authorization feature can block the users who without jwt toke in cookie

--- hw/src/main/java/com/esun/hw/config/SecurityConfig.java
+++ hw/src/main/java/com/esun/hw/config/SecurityConfig.java
@@ -2,27 +2,35 @@ package com.esun.hw.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import com.esun.hw.security.JwtFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private JwtFilter jwtFilter;
+
     @Bean
-    protected SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    protected SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)
+            throws Exception {
         httpSecurity
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(
-                        authz -> authz.requestMatchers("/api/auth/**")
-                                .permitAll().anyRequest().authenticated())
+                        authz -> authz.anyRequest().permitAll())
                 .httpBasic(withDefaults());
+
+        httpSecurity.addFilterBefore(this.jwtFilter,
+                BasicAuthenticationFilter.class);
 
         return httpSecurity.build();
     }
@@ -30,11 +38,5 @@ public class SecurityConfig {
     @Bean
     protected PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(
-            AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
     }
 }
---
+++ hw/src/main/java/com/esun/hw/security/JwtFilter.java
@@ -0,0 +1,100 @@
+package com.esun.hw.security;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.esun.hw.dao.UserRepository;
+import com.esun.hw.dto.request.JwtPayload;
+import com.esun.hw.exception.AppException;
+import com.esun.hw.model.User;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    private JwtProvider jwtProvider;
+
+    private UserRepository userRepository;
+
+    ObjectMapper objectMapper;
+
+    private String jwt;
+
+    private final Set<String> excludedUrls = new HashSet<>();
+
+    public JwtFilter(JwtProvider jwtProvider, ObjectMapper objectMapper, UserRepository userRepository) {
+        this.jwtProvider = jwtProvider;
+        this.objectMapper = objectMapper;
+        this.userRepository = userRepository;
+        excludedUrls.add("/api/auth/login");
+        excludedUrls.add("/api/auth/register");
+
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException, AppException {
+
+        try {
+            Cookie[] cookies = request.getCookies();
+            if (cookies == null) {
+                throw new AppException(HttpStatus.UNAUTHORIZED, "Invalid token");
+            }
+
+            for (Cookie cookie : cookies) {
+                if ("jwt".equals(cookie.getName())) {
+                    this.jwt = cookie.getValue();
+                    break;
+                }
+            }
+            User reqUser = getRequestUser(jwt);
+
+            if (reqUser == null) {
+                throw new AppException(HttpStatus.UNAUTHORIZED, "User Not found");
+            }
+
+            filterChain.doFilter(request, response);
+        } catch (AppException e) {
+            ErrorResponse errorResponse = ErrorResponse.builder(e, e.getStatus(), e.getErrorMessage()).build();
+            response.setContentType("application/json");
+            response.setStatus(e.getStatus().value());
+            response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+        }
+
+    }
+
+    private User getRequestUser(String token) throws JsonMappingException, JsonProcessingException {
+
+        JwtPayload payload = jwtProvider.decodedJwt(token);
+
+        if (payload == null) {
+            return null;
+        }
+
+        User reqUser = userRepository.findById(payload.getUserId()).get();
+
+        return reqUser;
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        // TODO Auto-generated method stub
+        String requestURI = request.getRequestURI();
+        return excludedUrls.contains(requestURI);
+    }
+
+}
